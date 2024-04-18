7805aed1faf06affd4c2c6aa3a2fd7f26bd357ae
Crowdbotics <team@crowdbotics.com>
2024-04-18 16:47:45 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-wind-47591",
+  url: "https://delicate-wind-47591.botics.co",
+  api: "https://delicate-wind-47591.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
